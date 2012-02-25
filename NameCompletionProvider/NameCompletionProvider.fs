820ac70cd6f68c7cd8bf2ce41e042326f9622e72
vosen <vosen@vosen.pl>
2012-02-25 19:30:04 +0100

    Add hallo world kind of webservice.

--- NameCompletionProvider/NameCompletionProvider.fs
+++ NameCompletionProvider/NameCompletionProvider.fs
@@ -1,6 +1,12 @@
-﻿// Learn more about F# at http://fsharp.net
-
-module NameCompletionProvider
+﻿namespace Vosen.Madarame
 
 open System
 open ServiceStack.ServiceHost
+
+type SearchQuery = { term : string }
+type Anime = { Name : string; Id : int }
+
+type NameCompletionService() = 
+    interface IService<SearchQuery> with
+        member this.Execute (query) = 
+            [| { Name = query.term; Id = 100 } |] :> obj
---
+++ NameCompletionProvider/run_service.fsx
@@ -0,0 +1,26 @@
+﻿// ugly workaround for vs2010
+#I @"bin\Debug\"
+
+#r "NameCompletionProvider.dll"
+#r "ServiceStack.dll"
+#r "ServiceStack.Interfaces.dll"
+
+open Vosen.Madarame
+open System
+open ServiceStack.WebHost.Endpoints
+
+type AppHost =
+    inherit AppHostHttpListenerBase
+    new() = { inherit AppHostHttpListenerBase("NameCompletionService", typeof<NameCompletionService>.Assembly) }
+    override this.Configure container =
+        base.Routes
+            .Add<SearchQuery>("/test") |> ignore 
+
+let main() =
+    let appHost = new AppHost()
+    appHost.Init()
+    appHost.Start("http://localhost:8080/")
+    printfn "listening on http://localhost:8080/ ..."
+    Console.ReadLine() |> ignore
+
+main()
