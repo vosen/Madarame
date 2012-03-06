69bfffb45d6d19c65a2650cbf375609094e3ce97
vosen <vosen@vosen.pl>
2012-03-06 12:04:24 +0100

    Add completion service to frontend.

---
+++ Web/Frontend/CompletionHost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack.WebHost.Endpoints;
+using Vosen.Madarame;
+
+namespace Web
+{
+    public class CompletionHost : AppHostBase
+    {
+        public CompletionHost()
+            : base("Test", typeof(NameCompletionService).Assembly)
+        { }
+
+        public override void Configure(Funq.Container container)
+        {
+            container.Register(c => new NameCompletionService("Server=127.0.0.1;Port=5432;Database=mal-utf8;User Id=vosen;Password=postgres;"));
+        }
+    }
+}
--- Web/Frontend/Global.asax
+++ Web/Frontend/Global.asax
@@ -2,6 +2,8 @@
 <script Language="C#" RunAt="server">
 
   protected void Application_Start(Object sender, EventArgs e) {
+      RouteTable.Routes.IgnoreRoute("api/{*pathInfo}");
+      new Web.CompletionHost().Init();
       base.Start();
   }
 
