d30bfa9c03102e4e5304b8979e93154401697329
vosen <vosen@vosen.pl>
2012-03-07 11:50:52 +0100

    Make name completion work.

--- Web/Frontend/CompletionHost.cs
+++ Web/Frontend/CompletionHost.cs
@@ -4,18 +4,29 @@ using System.Linq;
 using System.Web;
 using ServiceStack.WebHost.Endpoints;
 using Vosen.Madarame;
+using System.Configuration;
+using ServiceStack.ServiceHost;
 
 namespace Web
 {
     public class CompletionHost : AppHostBase
     {
         public CompletionHost()
-            : base("Test", typeof(NameCompletionService).Assembly)
+            : base("Title completion", typeof(NameCompletionService).Assembly)
         { }
 
         public override void Configure(Funq.Container container)
         {
-            container.Register(c => new NameCompletionService("Server=127.0.0.1;Port=5432;Database=mal-utf8;User Id=vosen;Password=postgres;"));
+            string connString = ConfigurationManager.ConnectionStrings[0].ConnectionString;
+            container.Register(c => new NameCompletionService(connString));
+#if !DEBUG
+            SetConfig(new EndpointHostConfig()
+            {
+                DebugMode = false,
+                EnableFeatures = Feature.Json,
+                AllowJsonpRequests = false
+            });
+#endif
         }
     }
 }
