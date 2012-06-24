beacc3a5e1930f97e1598a0a9122b35280f831b5
vosen <vosen@vosen.pl>
2012-06-24 22:53:24 +0200

    Preliminary bindings to native numerics libraries.

---
+++ NativeSvd/Native.fs
@@ -0,0 +1,46 @@
+﻿module internal Vosen.Madarame.Numerics.SafeNativeMethods
+
+open System.Runtime.InteropServices 
+open Microsoft.FSharp.NativeInterop
+
+[<StructLayoutAttribute(LayoutKind.Sequential)>]
+type smat =
+    struct
+        val mutable rows : int
+        val mutable cols : int
+        val mutable vals : int
+        val mutable pointr : nativeptr<int>
+        val mutable rowind : nativeptr<int>
+        val mutable value : nativeptr<float>
+        new(rows, cols, vals, pointr, rowind, value) =
+            { rows = rows; cols = cols; vals = vals; pointr = pointr; rowind = rowind; value = value }
+    end
+
+[<StructLayoutAttribute(LayoutKind.Sequential)>]
+type dmat =
+    struct
+        val mutable rows : int
+        val mutable cols : int
+        // double**
+        val mutable value : nativeptr<nativeptr<double>>
+    end
+
+[<StructLayoutAttribute(LayoutKind.Sequential)>]
+type svdrec =
+    struct
+        val mutable d : int
+        // dmat*
+        val mutable Ut : nativeptr<dmat>
+        // double*
+        val mutable S : nativeptr<float>
+        // dmat*
+        val mutable Vt : nativeptr<dmat>
+    end
+
+type svdrec_ptr = nativeptr<svdrec> 
+
+[<DllImport("svdlibc", CallingConvention = CallingConvention.Cdecl)>]
+extern svdrec_ptr svdLAS2A(smat& A, int dimensions)
+
+[<DllImport("svdlibc", CallingConvention = CallingConvention.Cdecl)>]
+extern void svdFreeSVDRec(svdrec_ptr svd)
---
+++ NativeSvd/NativeSvd.fs
@@ -0,0 +1,35 @@
+﻿namespace Vosen.Madarame.Numerics
+
+open Microsoft.FSharp.NativeInterop
+open Microsoft.FSharp.Math
+open SafeNativeMethods
+open System
+
+type NativeSvd =
+    val Vt : CMatrix<float>
+    val Ut : CMatrix<float>
+    val S : NativeArray<float>
+    val private SvdPtr : nativeptr<svdrec>
+
+    new(matrix: Matrix<float>, dims : int) =
+        let pinnedColumns = PinnedArray.of_array matrix.InternalSparseColumnValues
+        let pinnedRows = PinnedArray.of_array matrix.InternalSparseRowOffsets
+        let pinnedValues = PinnedArray.of_array matrix.InternalSparseValues
+        let mutable nativeMat = new smat(matrix.NumCols, matrix.NumRows, matrix.InternalSparseValues.Length, pinnedRows.Ptr, pinnedColumns.Ptr, pinnedValues.Ptr)
+        pinnedColumns.Free(); pinnedRows.Free(); pinnedValues.Free()
+        let svd_ptr = svdLAS2A(&nativeMat, dims)
+        let svd_struct = svd_ptr |> NativePtr.read
+        let vt_struct = svd_struct.Vt |> NativePtr.read
+        let ut_struct = svd_struct.Ut |> NativePtr.read
+        // weird assignment because Matrix<float> and svdlibc use different row/column formats
+        { Ut = CMatrix(vt_struct.value |> NativePtr.read, vt_struct.rows, vt_struct.cols);
+          Vt = CMatrix(ut_struct.value |> NativePtr.read, ut_struct.rows, ut_struct.cols);
+          S = NativeArray(svd_struct.S, svd_struct.d);
+          SvdPtr = svd_ptr }
+
+    member this.Dispose() =
+        svdFreeSVDRec(this.SvdPtr)
+
+    interface IDisposable with
+        member this.Dispose() =
+            this.Dispose()
