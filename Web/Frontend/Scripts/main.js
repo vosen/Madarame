745a205d5272f82c33510a7929d3b9402a8bfcf9
vosen <vosen@vosen.pl>
2012-08-24 11:52:19 +0200

    Redone start page slightly to pronounce "Add" button more.

--- Web/Frontend/Scripts/main.js
+++ Web/Frontend/Scripts/main.js
@@ -1,3 +1,4 @@
+var add_rating_row;
 $(document).ready(function () {
     var row_counter = 0;
     var title_row = '<tr><td><input class="input typeahead" name="title" type="text" data-provide="typeahead"><input name="id" type="hidden"></td><td><select name="rating"><option>10</option><option>9</option><option>8</option><option>7</option><option>6</option><option>5</option><option>4</option><option>3</option><option>2</option><option>1</option></select></td><td><button class="btn btn-danger button-del disabled" disabled="disabled"><i class="icon-white icon-minus"></i></button></td></tr>';
@@ -27,6 +28,23 @@ $(document).ready(function () {
         }
         event.preventDefault();
     };
+    add_rating_row = function (name, id, rating) {
+        row_counter++;
+        var title_body = $('#title-list tbody');
+        var $title_row = $(title_row);
+        $title_row.find('.typeahead').val(name);
+        $title_row.find('input[type="hidden"]').val(id);
+        $title_row.find('select').val(rating);
+        title_body.append(title_row);
+        var typeahead = title_body.find('tr:last .typeahead').typeahead(typeahead_handler);
+        $('.typeahead').on('change', function () {
+            $(this).siblings().val(undefined);
+        });
+        title_body.find('tr:last .button-del').click(del_handler);
+        if (row_counter > 1) {
+            $('#title-list .button-del').removeClass('disabled').removeAttr('disabled');
+        }
+    };
     var add_handler = function (event) {
         row_counter++;
         var title_body = $('#title-list tbody');
@@ -42,8 +60,11 @@ $(document).ready(function () {
         if (event !== undefined)
             event.preventDefault();
     };
-    add_handler();
-    $('.button-add').click(add_handler);
+    add_rating_row('', 0, 10);
+    $('.button-add').click(function (event) {
+        add_rating_row('', 0, 10);
+        event.preventDefault();
+    });
     // jscript for rec list
     var title_add_button = '<button class="btn btn-success button-seen"><i class="icon-white icon-plus"></i> Seen it</button>';
     var title_del_float = '<span class="float-seen-del"><input class="input" name="title" type="hidden"><input name="id" type="hidden"></td><td><select name="rating"><option>10</option><option>9</option><option>8</option><option>7</option><option>6</option><option>5</option><option>4</option><option>3</option><option>2</option><option>1</option></select></td><td><button class="btn btn-danger"><i class="icon-white icon-minus"></i></button></td></tr></span>';
